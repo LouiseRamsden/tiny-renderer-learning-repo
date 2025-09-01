4159ffca33acf282c268053ca168ee6eed92f397
NoteBot <louiseramsden2@gmail.com>
2025-09-01 11:47:49 +0100

    a few colours

--- tiny-renderer-learning-repo/gfxlib.h
+++ tiny-renderer-learning-repo/gfxlib.h
@@ -5,6 +5,9 @@ const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
 const TGAColor blue = TGAColor(0, 0, 255, 255);
 const TGAColor green = TGAColor(0, 255, 0, 255);
+const TGAColor cyan = TGAColor(0, 255, 255, 255);
+const TGAColor yellow = TGAColor(255, 255, 0, 255);
+const TGAColor magenta = TGAColor(255, 0, 255, 255);
 
 //draw line from point a to point b on framebuffer
 void line(int ax, int ay, int bx, int by, TGAImage& framebuffer, TGAColor const& color);
--- tiny-renderer-learning-repo/main.cpp
+++ tiny-renderer-learning-repo/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char** argv)
 	TGAImage image(500, 500, TGAImage::RGB);
 	line(4, 4, 80, 20, image, green);
 	line(1, 1, 70, 99, image, red);
-	line(50, 50, 0, 0, image, blue);
+	line(50, 50, 0, 0, image, magenta);
 	image.flip_vertically(); // set origin to bottom left
 	image.write_tga_file("output//output.tga");
 	return 0;
