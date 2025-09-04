139038eed1bf848f03cc44da114529d51cae74ea
NoteBot <louiseramsden2@gmail.com>
2025-09-04 09:41:23 +0100

    moved to cpp17

--- tiny-renderer-learning-repo/main.cpp
+++ tiny-renderer-learning-repo/main.cpp
@@ -1,11 +1,23 @@
+#include <tuple>
+
 #include "tgaimage.h"
 #include "gfxlib.h"
 #include "model.h"
+
 
 constexpr int g_width = 2000;
 constexpr int g_height = 2000;
 Model* model = nullptr;
 
+
+std::tuple<int, int, int> projectTriangleToScreen(Vec3f v, int width, int height)
+{
+	return { 
+		(int)(((v.x + 1.0f) * width) / 2.0f),
+		(int)(((v.y + 1.0f) * height) / 2.0f),
+		(int)(((v.z + 1.0f) * 255) / 2.0f)
+	};
+}
 int main(int argc, char** argv) 
 {
 
@@ -17,14 +29,12 @@ int main(int argc, char** argv)
 	for (int i = 0; i < model->nfaces(); i++)
 	{
 		std::vector<int> face = model->face(i);
-		Vec3f a, b, c;
-		a = model->vert(face[0]);
-		b = model->vert(face[1]);
-		c = model->vert(face[2]);
-		triangle(
-			(a.x + 1.0f) * g_width/2.0f, (a.y + 1.0f) * g_height / 2.0f, (a.z + 1) * 255 / 2.0f,
-			(b.x + 1.0f) * g_width / 2.0f, (b.y + 1.0f) * g_height / 2.0f, (b.z + 1) * 255 / 2.0f, 
-			(c.x + 1.0f) * g_width / 2.0f, (c.y + 1.0f) * g_height / 2.0f, (c.z + 1) * 255 / 2.0f, 
+
+		auto [ax, ay, az] = projectTriangleToScreen(model->vert(face[0]), g_width, g_height);
+		auto [bx, by, bz] = projectTriangleToScreen(model->vert(face[1]), g_width, g_height);
+		auto [cx, cy, cz] = projectTriangleToScreen(model->vert(face[2]), g_width, g_height);
+
+		triangle(ax,ay,az,bx,by,bz,cx,cy,cz, 
 			zbuffer,
 			framebuffer, 
 			genRandColor());
